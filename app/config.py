c0f913d464d730219e3134608ee6b222c71acf31
VaigundaRaJa345 <149135689+VaigundaRaJa345@users.noreply.github.com>
2025-03-21 09:30:37 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost/quick_aid'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://quick_care_user:0SM3EPvLjGsVr8LCFNPcotpNahYME0pH@dpg-cv5bp5q3esus73aridg0-a.oregon-postgres.render.com/quick_care'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
